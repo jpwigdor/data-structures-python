0bb720ea3c7fe6812b396be5e88c0742bce100b5
James Wigdor <jpwigdor@gmail.com>
2020-06-10 15:30:47 -0400

    complete binary search tree pt.1

--- RubberDuck.py
+++ RubberDuck.py
@@ -1,7 +1,25 @@
 """
-Questions to ask your
+Questions to explain to your duck
 explain: 
 
 
 
 """
+
+
+def binarySearch(listData, value):
+    low = 0
+    high = len(listData) - 1
+    while (low <= high):
+        mid = (low + high) / 2
+        if (listData[mid] == value):
+            return mid
+        elif (listData[mid] < value):
+            low = mid + 1
+        else:
+            high = mid - 1
+    return -1
+
+
+#recursion example
+
